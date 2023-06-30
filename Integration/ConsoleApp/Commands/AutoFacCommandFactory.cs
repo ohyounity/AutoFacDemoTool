bcb435bf322207ccc146cfc6792204c2b374dd7a
Christopher Wodarczyk <christopher.wodarczyk.business@gmail.com>
2023-06-29 18:39:12 -0700

    Initial commit

---
+++ Integration/ConsoleApp/Commands/AutoFacCommandFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using ConsoleApp.Framework.Commands;
+using ConsoleApp.Framework.User;
+
+namespace Console.Commands
+{
+    public class AutoFacCommandFactory : ICommandFactory
+    {
+        private readonly IEnumerable<ICommand> _commands;
+
+		private readonly HelpCommand _help;
+
+		public AutoFacCommandFactory(IEnumerable<ICommand> commands,
+			HelpCommand help)
+        {
+            _commands = commands;
+			_help = help;
+        }
+
+        public IDictionary<string, ICommand> CreateCommands(IUser currentUser)
+        {
+            var validCmds = _commands.Where(c => currentUser.AllowCommand(c)).ToDictionary((c) => c.Name.ToLower());
+			// Add help directly
+			validCmds.Add(_help.Name, _help);
+			return validCmds;
+        }
+    }
+}
---
+++ Integration/ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using ConsoleApp.Framework.CommandLine;
+using Microsoft.Extensions.Configuration;
+using Autofac.Configuration;
+using Console;
+
+namespace ConsoleApp
+{
+	class Program
+    {
+        static void Main(string[] args)
+        {
+			CommandLineOptions.Instance.ParseCommandLine(args, System.Console.Out);
+
+			// Add the configuration to the ConfigurationBuilder.
+			var config = new ConfigurationBuilder();
+			// config.AddJsonFile comes from Microsoft.Extensions.Configuration.Json
+			// config.AddXmlFile comes from Microsoft.Extensions.Configuration.Xml
+			config.AddJsonFile(@"Resources\config.json");
+
+			// Register the ConfigurationModule with Autofac.
+			var module = new ConfigurationModule(config.Build());
+			var builder = new ContainerBuilder();
+			builder.RegisterModule(module);
+
+			var container = builder.Build();
+
+			var shell = container.Resolve<MainShell>();
+			shell.Execute();
+		}
+    }
+}
